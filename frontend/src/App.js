78b87d0b78edf84f217e8ed455cdfaa53b80ef4c
Blu3Orang3 <matealtunashvili@gmail.com>
2022-10-10 19:22:19 +0400

    before payment gateway

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,40 @@
-
+import store from "./store";
+import { Provider } from "react-redux";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 
 function App() {
   return (
-    <>
-      <h1>Welcome to ProShop</h1>
-    </>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Routes>
+              <Route path='/shipping' element={<ShippingScreen />} />
+              <Route path='/login' element={<LoginScreen />} />
+              <Route path='/register' element={<RegisterScreen />} />
+              <Route path='/profile' element={<ProfileScreen />} />
+              <Route path='/profile/:id' element={<ProfileScreen />} />
+              <Route path='/product/:id' element={<ProductScreen />} />
+              <Route path='/cart' element={<CartScreen />} exact />
+              <Route path='/cart/:id' element={<CartScreen />} />
+              <Route path='/' element={<HomeScreen />} exact />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </Provider>
   );
 }
 
